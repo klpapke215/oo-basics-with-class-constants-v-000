2b7571b99c1ac69612b09ec1417bb2b399766de7
Kristen Papke <klpapke215@gmail.com>
2018-07-24 17:46:04 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
   end
@@ -14,6 +16,6 @@ class Shoe
   def brand=(brand)
     @brand = brand
     BRANDS << brand
-  end 
+  end
 
 end
